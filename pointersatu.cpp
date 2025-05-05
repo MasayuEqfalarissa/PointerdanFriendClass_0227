70cbb7ab180290072632b8c3d2078f5fbca957b2
MasayuEqfalarissa <masayueqfalarissaa@gmail.com>
2025-05-05 18:50:48 +0700

    add class

--- pointersatu.cpp
+++ pointersatu.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
 using namespace std;
+
+class mahasiswa 
+{
+public:
+    int nim;
+    void showtim()
+    {
+        cout << "no induk = " << nim << endl;
+    }
+};
+
+
+
+
+
+int main () 
+{
+
+}
