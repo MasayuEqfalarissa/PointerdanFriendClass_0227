395e7a7ee19239ff48cc8503b0a729fc9260a7fe
MasayuEqfalarissa <masayueqfalarissaa@gmail.com>
2025-05-05 19:10:41 +0700

    add library

--- pointersatu.cpp
+++ pointersatu.cpp
@@ -5,17 +5,26 @@ class mahasiswa
 {
 public:
     int nim;
-    void showtim()
+    void shownim()
     {
         cout << "no induk = " << nim << endl;
     }
 };
 
 
-
-
-
 int main () 
 {
+    mahasiswa mhs; // object mhs
+    mhs.nim = 2024;
+    mhs.shownim(); // member access operator 
+
+    mahasiswa &refmhs = mhs; // pointer reference refmhs
+    refmhs.nim = 2;
+    mhs.shownim();
+
+    mahasiswa *pmhs = &mhs;//pointer dereference pmhs;
+    pmhs -> nim =3;
+    pmhs ->shownim ();
+    return 0;
 
 }
