19e3a9990c367adcf24c2aeaf2e55a287636487d
MasayuEqfalarissa <masayueqfalarissaa@gmail.com>
2025-05-05 19:29:25 +0700

    add void

--- pointertiga.cpp
+++ pointertiga.cpp
@@ -20,5 +20,20 @@ class siswa
     public :
     void setid(int pid);
     void displayall(orang &a);
-
 };
+
+void orang::setnama (string pnama)
+{
+    nama = pnama;
+}
+
+void siswa :: setid(int pid)
+{
+    id = pid;
+}
+
+void siswa::displayall(orang &a)
+{
+    cout << id << endl
+        << a.nama;
+}
